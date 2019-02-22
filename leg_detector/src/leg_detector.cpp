aa9547f7ced18b37c7177fcb176ceba1136f3445
Monroe Kennedy <monroe.kennedy3@gmail.com>
2019-02-22 16:56:37 -0500

    changes add for fetch to work

--- leg_detector/src/leg_detector.cpp
+++ leg_detector/src/leg_detector.cpp
@@ -71,7 +71,7 @@ static double max_second_leg_age_s     = 2.0;
 static double max_track_jump_m         = 1.0;
 static double max_meas_jump_m          = 0.75; // 1.0
 static double leg_pair_separation_m    = 1.0;
-static string fixed_frame              = "odom_combined";
+static string fixed_frame              = "odom";
 
 static double kal_p = 4, kal_q = .002, kal_r = 10;
 static bool use_filter = true;
